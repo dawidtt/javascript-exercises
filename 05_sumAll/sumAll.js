a27cbbf54ddbb95286a34f16264de216459ad555
dawidtt <dawid.gorszka@gmail.com>
2024-07-12 17:47:44 +0200

    5 task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    let smaller;
+    let larger;
+    if (typeof firstNumber != "number" ||  typeof secondNumber != "number" || firstNumber < 0 || secondNumber < 0) return "ERROR";
+    
+    if (firstNumber > secondNumber) {
+        smaller = secondNumber;
+        larger = firstNumber;
+    }
+    else {
+        smaller = firstNumber;
+        larger = secondNumber;
+    }
+    
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
