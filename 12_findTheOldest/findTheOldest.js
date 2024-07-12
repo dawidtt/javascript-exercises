23348098c7117b094dc9d706917f623890bf8be7
dawidtt <dawid.gorszka@gmail.com>
2024-07-13 01:27:32 +0200

    12 task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let oldestPerson = {};
+  let oldestPersonAge = 0;
+  let currentAge = 0;
+  for (const person of people) {
+    if (person.yearOfDeath === undefined) {
+      const currentYear = new Date().getFullYear();
+      currentAge = currentYear - person.yearOfBirth;
+    } else {
+      currentAge = person.yearOfDeath - person.yearOfBirth;
+    }
+    if (currentAge > oldestPersonAge) {
+      oldestPersonAge = currentAge;
+      oldestPerson = person;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
