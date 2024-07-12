621c212bb4a70f9f3961ed9bfb5af339ee02456b
dawidtt <dawid.gorszka@gmail.com>
2024-07-12 18:00:19 +0200

    6 task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let bool
+    if (year % 400 === 0) {
+        bool = true;
+    }
+    else if (year % 100 === 0) {
+        bool = false;
+    }
+    else if (year % 4 === 0) {
+        bool = true;
+    }
+    else {
+        bool = false;
+    }
+    return bool;
 };
 
 // Do not edit below this line
