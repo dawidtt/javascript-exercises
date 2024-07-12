0b678c2222ca7db3d0b3d4ddff3d58d6ec72e6a7
dawidtt <dawid.gorszka@gmail.com>
2024-07-12 18:56:25 +0200

    9 task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+function removePunktuation(letter) {
+    if(letter !== "." && letter !== "," && letter !== " " && letter !== "!") return true;
+    else return false;
+}
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    const stringAsArray = string.split("");
+    let filteredStringAsArray = stringAsArray.filter(removePunktuation);
+    string = filteredStringAsArray.join("");
+    const reversedStringAsArray = filteredStringAsArray.reverse();
+    const reversedString = reversedStringAsArray.join("");
+    if (string === reversedString) return true;
+    else return false;
 };
 
 // Do not edit below this line
